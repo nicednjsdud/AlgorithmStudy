3fba09c6127e1f13f84081d9eb82bb78809bf9d3
nicednjsdud <nicednjsdud@naver.com>
2022-03-22 17:51:29 +0900

    BackJoon Algorithm
    Algorithm 2798,2822

---
+++ src/Back_2822.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Back_2822 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int arr[]=new int[8];
+        int arr2[]=new int[5];
+        int arr3[]=new int[8];
+        for(int i=0;i<arr.length;i++){
+            arr[i]=sc.nextInt();
+            arr3[i]=arr[i];
+        }
+        Arrays.sort(arr);
+        int sum=0;
+        for(int i=3;i<arr.length;i++){
+            arr2[i-3]=arr[i];
+            sum+=arr2[i-3];
+        }
+        System.out.println(sum);
+        for(int i=0;i<arr3.length;i++){
+            for(int j=0;j<arr2.length;j++){
+                if(arr3[i]==arr2[j]){
+                    System.out.print((i+1)+" ");
+                }
+            }
+        }
+        System.out.println();
+        sc.close();
+    }
+}
