55f18bce5559804c7426aa297312d94cacd7e40b
nicednjsdud <nicednjsdud@naver.com>
2022-03-24 17:15:19 +0900

    add BackJoon.java
    BackJoon Algorithm 2947, 2953

---
+++ src/Back_2953.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Back_2953 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // 입력
+        int num=4;
+        int sum=0;
+        // 배열로 담기
+        int arr[][]=new int[5][4];
+
+        for(int i=0;i<num;i++){
+            sum=0;
+            for(int j=0;j<num;j++){
+                arr[j]
+            }
+        }
+
+
+    }
+}
