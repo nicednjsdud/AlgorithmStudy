8ad35c2967e30ee00b8256978ec6927c78cebefd
nicednjsdud <nicednjsdud@naver.com>
2022-03-28 17:14:49 +0900

    Update Back_3046
    BackJoon Algorithm study hard!

---
+++ src/Back_3046.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Back_3046 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        // 입력
+        int R1= sc.nextInt();
+        int S=sc.nextInt();
+        int R2=(S*2)-R1;
+
+        System.out.println(R2);
+    }
+}
