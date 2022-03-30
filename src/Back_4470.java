00f8bbd7f64800a75a5c7c8136e1f214468d077c
nicednjsdud <nicednjsdud@naver.com>
2022-03-30 09:28:01 +0900

    Update Back_4470.java
    BackJoon  morning Algorithm

---
+++ src/Back_4470.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Back_4470 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int num=sc.nextInt();
+        sc.nextLine();
+        String str[]=new String[num];
+        for(int i=0;i<num;i++){
+            str[i]=sc.nextLine();
+        }
+        for(int i=0;i<num;i++){
+            System.out.println((i+1)+". "+str[i]);
+        }
+    }
+}
