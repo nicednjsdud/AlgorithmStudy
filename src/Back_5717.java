5691493d0b698ddd91221cfe2d929404ba431528
nicednjsdud <nicednjsdud@naver.com>
2022-04-11 09:18:11 +0900

    Create Back_5717.java
    BackJoon Morning Algorithm

---
+++ src/Back_5717.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Back_5717 {
+    public static void main(String[] args) {
+
+        // 입력
+        Scanner sc = new Scanner(System.in);
+
+        while(true){
+            int boy=sc.nextInt();
+            int girl= sc.nextInt();
+            if(boy==0 && girl==0){
+                break;
+            }
+            int sum=boy+girl;
+            System.out.println(sum);
+        }
+    }
+}
