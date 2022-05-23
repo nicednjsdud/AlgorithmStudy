2bc89405d7a83651dc297bd694cb40ba52738521
nicednjsdud <nicednjsdud@naver.com>
2022-05-23 18:44:40 +0900

    BackJoon Algorithm study

--- src/Back_3004.java
+++ src/Back_3004.java
@@ -8,8 +8,19 @@ public class Back_3004 {
         // given
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
+        int count_sum = 0;      // 조각 개수
         // when
+        for(int i=1;i<=N;i++){
+            if( i == 1){
+                count_sum +=2;          // 처음 값으로 2셋팅
+            }
+            else{
+                count_sum +=((i/2)+1);  //
+            }
 
+        }
+        System.out.println(count_sum);
+        br.close();
     }
 
 }
---
+++ src/Back_3035.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Back_3035 {
+    public static void main(String[] args) throws IOException {
+
+        // given
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer token = new StringTokenizer(br.readLine());
+        int row = Integer.parseInt(token.nextToken());      // 행입력
+        int column = Integer.parseInt(token.nextToken());   // 열입력
+        int ZR = Integer.parseInt(token.nextToken());       // 행으로 늘릴 배수
+        int ZC = Integer.parseInt(token.nextToken());       // 열로 늘릴 배수
+        // when
+        for(int i=0;i<row;i++){
+            for(int j=0;j<column;j++){
+                token= new StringTokenizer(br.readLine());
+                
+            }
+        }
+
+        // then
+    }
+}
