1600c21f31ae3ac63db53e911c14145c786ac780
JeongWonYoung <nicednjsdud@naver.com>
2022-08-01 17:48:22 +0900

    Solve algorithm decimal 1929
    solve backjoon algorithm about decimal with
    Eratosthenes sieve

--- src/Back_1929.java
+++ src/Back_1929.java
@@ -4,7 +4,9 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
 
+// 에라토스테네스 체 방법으로 풀어보기
 public class Back_1929 {
+
     public static void main(String[] args) throws IOException {
 
         // given
@@ -13,24 +15,24 @@ public class Back_1929 {
         StringBuilder sb = new StringBuilder();
         int M = Integer.parseInt(token.nextToken());
         int N = Integer.parseInt(token.nextToken());
-        ArrayList<Integer> list = new ArrayList<>();
-        for(int i=M;i<=N;i++){
-            if (i % 2 != 0) {
-                for (int j = 2; j <= i; j++) {
-                    if (j == i) {
-                        list.add(i);
-                    }
-                    if (i % j == 0) {
-                        break;
-                    }
-                }
-            }
+        boolean arr[] = new boolean[N + 1];
+        // when
+        arr[0] = true;
+        arr[1] = true;        // 0 하고 1은 빼주기
 
+        for (int i = 2; i < Math.sqrt(N + 1); i++) {
+            for (int j = i * i; j < N + 1; j += i) {
+                arr[j] = true;
+            }
         }
         // then
         br.close();
-        for (Integer integer : list) {
-            System.out.println(integer);
+        for (int i = M; i < N + 1; i++) {
+            if (arr[i] == false) {
+                System.out.println(i);
+            }
         }
+
+
     }
 }
