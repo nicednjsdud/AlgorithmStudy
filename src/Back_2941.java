6962710f8a6e814ea6df05d9eb8e5313ddfeff21
JeongWonYoung <nicednjsdud@naver.com>
2022-10-17 12:01:39 +0900

    Solve algorithm back 2941

---
+++ src/Back_2941.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Back_2941 {
+    public static void main(String[] args) throws IOException {
+
+        // given
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        String cro_alphabet[] = {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
+        // when
+        for(int i=0;i <cro_alphabet.length;i++){
+            if(str.contains(cro_alphabet[i])){
+                str = str.replace(cro_alphabet[i], "@");
+            }
+        }
+        
+        // then
+        System.out.println(str.length());
+        br.close();
+    }
+}
