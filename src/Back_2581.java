f073a5f3f8c0e4a39c3a0e33c68fcb6010f8e52b
JeongWonYoung <nicednjsdud@naver.com>
2022-10-21 11:33:41 +0900

    Fix algorithm 2581

--- src/Back_2581.java
+++ src/Back_2581.java
@@ -9,42 +9,44 @@ public class Back_2581 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int M = Integer.parseInt(br.readLine());
         int N = Integer.parseInt(br.readLine());
+
+        // N 배열 생성
+        boolean prime[] = new boolean[N + 1];
+
+        // 0하고 1은 소수가 아님
+        prime[0] = true;
+        prime[1] = true;
+
+        // 에라토스테네스 체
+        for (int i = 2; i <= Math.sqrt(prime.length); i++) {
+            for (int j = i * i; j < prime.length; j += i) {
+                prime[j] = true;
+            }
+        }
         int sum = 0;
         int small_count = 0;
         // when
         for (int i = M; i <= N; i++) {
 
-            int count = 0;
-            for (int j = 3; j < i; j++) {
+            // i 가 소수이면
+            if (prime[i] == false) {
+                sum += i;
 
-                if (i % j != 0) {
-                   continue;
-                } else {
-                    count++;
-                    break;
-                }
-
-            }
-            // 소수라면
-            if (count == 0) {
+                // 첫 소수는 최소값
                 if (small_count == 0) {
                     small_count = i;
-                    sum += small_count;
-                } else {
-                    // 최소값 구하기
-                    sum += i;
-                    small_count = Math.min(i, small_count);
                 }
             }
         }
+
         // then
-        if(sum == 0 && small_count == 0){
+        if (sum == 0 && small_count == 0) {
             System.out.println(-1);
-        }
-        else {
+        } else {
             System.out.println(sum);
             System.out.println(small_count);
         }
+
         br.close();
     }
 }
