b26d5e6bbe08596a8854d7922f818a5d1a2d3090
JeongWonYoung <nicednjsdud@naver.com>
2022-10-27 13:39:07 +0900

    Solve back algorithm

--- src/Back_2566.java
+++ src/Back_2566.java
@@ -1,35 +1,36 @@
 
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
-/*
-     같이 9×9 격자판에 쓰여진 81개의 자연수 또는 0이 주어질 때,
-     이들 중 최댓값을 찾고 그 최댓값이 몇 행 몇 열에 위치한 수인지 구하는 프로그램을 작성하시오.
- */
 public class Back_2566 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-      //     given
-        int max=0;
-        int arr[][]=new int[9][9];
-        int row=0;
-        int col=0;
+    public static void main(String[] args) throws IOException {
+
+        //     given
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int max = 0;
+        int arr[][] = new int[9][9];
+        int row = 0;
+        int col = 0;
         //    when
-        for(int i=0;i<9;i++){
-            for(int j=0;j<9;j++){
-                arr[i][j] = sc.nextInt();
-                if(max<arr[i][j]){
-                    max=arr[i][j];
-                    row=i+1;
-                    col=j+1;
+        for (int i = 0; i < 9; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 9; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+                if (max < arr[i][j]) {
+                    max = arr[i][j];
+                    row = i;
+                    col = j;
                 }
             }
         }
         //    then
         System.out.println(max);
-        System.out.println(row+" "+col);
-        sc.close();
-
-
+        System.out.println((row+1) + " " + (col+1));
+        br.close();
     }
 }
