9bba72b29bfb92ff0effd477cba50fdab014df1d
JeongWonYoung <nicednjsdud@naver.com>
2022-11-17 21:17:23 +0900

    Solve algorithm

--- src/Back_3035.java
+++ src/Back_3035.java
@@ -1,47 +1,49 @@
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 
 public class Back_3035 {
     public static void main(String[] args) throws IOException {
 
         // given
-        Scanner sc = new Scanner(System.in);
-
-        List<String> list = new ArrayList<>();
-        String[] str = sc.nextLine().split(" ");
-        int[] arr = new int[str.length];
-        //when
-        for(int i=0;i<arr.length;i++){
-            arr[i]=Integer.parseInt(str[i]);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        int R = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+        int ZR = Integer.parseInt(st.nextToken());
+        int ZC = Integer.parseInt(st.nextToken());
+        String[] str = new String[R];
+        for (int i = 0; i < R; i++) {
+            str[i] = br.readLine();
         }
-        while(sc.hasNextLine()){
-            String book = sc.nextLine();
-            if(book ==""){
-                break;
+        // when
+        // 먼저 좌우 확대부터 확대하기
+        if (ZC > 1) {
+            for (int i = 0; i < R; i++) {
+                String temp = "";
+                for (int j = 0; j < C; j++) {
+                    for (int k = 0; k < ZC; k++) {
+                        temp += str[i].charAt(j);
+                    }
+                }
+                str[i] = temp;
             }
-            list.add(book);
         }
-
-        int x = arr[1]* arr[3] / list.get(0).length();
-        int y = arr[0]* arr[2] / list.size();
+        // 위아래 확대하기
+        for (int i = 0; i < R; i++) {
+            for (int j = 0; j < ZR; j++) {
+                sb.append(str[i]).append("\n");
+            }
+        }
         // then
-        for(int i=0;i<list.size();i++){
-            String str1="";
-            for(int j=0;j<list.get(0).length();j++){
-                for(int k=0;k<x;k++){
-                    str1 +=list.get(i).charAt(j);
-                }
-                for(int k=0;k<y;k++){
-                    System.out.println(str1);
-                    System.out.println();
-                }
-            }
-        }
-
-
+        System.out.println(sb);
+        br.close();
     }
 }
