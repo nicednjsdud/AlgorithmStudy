f76274f73fe20cf36fa884bc11c27a4b2547b9dd
JeongWonYoung <nicednjsdud@naver.com>
2023-03-24 23:48:28 +0900

    Solve algorithm

---
+++ src/Back_5037.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Back_5037 {
+    public static void main(String[] args) throws IOException {
+
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        boolean checkFlag = true;
+        StringBuilder sb = new StringBuilder();
+        while (!checkFlag) {
+           st = new StringTokenizer(br.readLine(), " ");
+           int first_num = Integer.parseInt(st.nextToken());
+           int second_num = Integer.parseInt(st.nextToken());
+           int third_num = Integer.parseInt(st.nextToken());
+
+           if(first_num == 0 && second_num == 0 && third_num == 0){
+               checkFlag = false;
+               return;
+            }
+
+           if(first_num == second_num){
+               if(second_num == third_num){
+                   sb.append("Equilateral").append("\n");
+               }
+               else{
+                   sb.append("Isosceles").append("\n");
+               }
+           }
+           else{
+               if(second_num == third_num){
+                   sb.append("Isosceles").append("\n");
+               }
+               else if(first_num == third_num){
+                   sb.append("Isosceles").append("\n");
+               }
+               else{
+                   sb.append("Isosceles").append("\n");
+               }
+           }
+        }
+    }
+}
