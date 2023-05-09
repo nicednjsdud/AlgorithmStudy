a84fb4affb32875fe1abbe44562fcfd45b80264c
JeongWonYoung <nicednjsdud@naver.com>
2023-05-09 23:35:49 +0900

    Solve algorithm

---
+++ src/Back_1026.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class Back_1026 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        Integer arrA[] = new Integer[N];
+        Integer arrB[] = new Integer[N];
+
+        for (int i = 0; i < N; i++) {
+            arrA[i] = Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arrB[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arrA);
+        Arrays.sort(arrB, Collections.reverseOrder());
+
+        int sum = 0;
+        for (int i = 0; i < N; i++) {
+            sum += arrA[i] * arrB[i];
+        }
+        System.out.println(sum);
+        br.close();
+    }
+}
