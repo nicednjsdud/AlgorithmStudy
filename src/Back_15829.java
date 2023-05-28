9040602d40cedf2a0be4a21491230c367a5edd5b
JeongWonYoung <nicednjsdud@naver.com>
2023-05-28 21:57:11 +0900

    change int -> long

--- src/Back_15829.java
+++ src/Back_15829.java
@@ -8,10 +8,10 @@ public class Back_15829 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int L = Integer.parseInt(br.readLine());
         String str = br.readLine();
-        int pow = 1;
-        int result = 0;
+        long pow = 1;
+        long result = 0;
         for (int i = 0; i < L; i++) {
-            result += ((str.charAt(i) - 96) * pow);
+            result += ((str.charAt(i) - 96) * pow) % 1234567891;
             pow = (pow * 31) % 1234567891;
         }
         System.out.println(result % 1234567891);
