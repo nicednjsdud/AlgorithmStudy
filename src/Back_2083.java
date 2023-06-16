7cc2b614ab38245a94c1b1c0ca7bc4de5ee7a829
JeongWonYoung <nicednjsdud@naver.com>
2023-06-16 23:43:57 +0900

    Solve algorithm

---
+++ src/Back_2083.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Back_2083 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        while (true) {
+            st = new StringTokenizer(br.readLine());
+            String str = st.nextToken();
+            int age = Integer.parseInt(st.nextToken());
+            int weight = Integer.parseInt(st.nextToken());
+            if (str.equals("#") && age == 0 && weight == 0) {
+                break;
+            }
+            if (age > 17 || weight >= 80) {
+                sb.append(str).append(" ").append("Senior").append("\n");
+            } else {
+                sb.append(str).append(" ").append("Junior").append("\n");
+            }
+        }
+        System.out.println(sb);
+        br.close();
+    }
+}
