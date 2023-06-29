b663bc13feb303990f01c2abfa3d05dd0e2bce55
JeongWonYoung <nicednjsdud@naver.com>
2023-06-29 23:21:32 +0900

    Upload post

---
+++ src/Back_2914.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Back_2914 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int I = Integer.parseInt(st.nextToken());
+
+        System.out.println(A * (I -1) + 1);
+        br.close();
+    }
+}
