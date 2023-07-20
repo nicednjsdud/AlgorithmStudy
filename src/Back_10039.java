af416dbb272061b6f8133da1198082033d052549
JeongWonYoung <nicednjsdud@naver.com>
2023-07-20 23:52:55 +0900

    Solve algorithm

---
+++ src/Back_10039.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Back_10039 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int sum = 0;
+        for (int i = 0; i < 5; i++) {
+            int num = Integer.parseInt(br.readLine());
+            sum += num;
+        }
+        System.out.println(sum/5);
+    }
+}
