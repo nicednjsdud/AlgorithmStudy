2c64e08e836767447cd0a636b349c5325970e81f
JeongWonYoung <nicednjsdud@naver.com>
2023-09-10 23:46:03 +0900

    Solve algorithm

---
+++ src/Back_1389.java
@@ -0,0 +1,63 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Back_1389 {
+
+    static final int VALUE = 987654321;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[][] arr = new int[N + 1][N + 1];
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                arr[i][j] = VALUE;
+
+                if (i == j) {
+                    arr[i][j] = 0;
+                }
+            }
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            arr[x][y] = arr[y][x] = 1;
+        }
+
+        for (int k = 1; k <= N; k++) {
+            for (int i = 1; i <= N; i++) {
+                for (int j = 1; j <= N; j++) {
+                    if (arr[i][j] > arr[i][k] + arr[k][j]) {
+                        arr[i][j] = arr[i][k] + arr[k][j];
+                    }
+                }
+            }
+        }
+
+        int res = VALUE;
+        int idx = -1;
+
+        for (int i = 1; i <= N; i++) {
+            int total = 0;
+            for (int j = 1; j <= N; j++) {
+                total += arr[i][j];
+            }
+
+            if (res > total) {
+                res = total;
+                idx = i;
+            }
+        }
+
+        System.out.println(idx);
+        br.close();
+    }
+}
