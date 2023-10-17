3c59c98171ee515b53498e29bac3b40683f29e80
JeongWonYoung <nicednjsdud@naver.com>
2023-10-17 23:46:16 +0900

    Solve algorithm

---
+++ src/Back_1977.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Back_1977 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int m = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());  
+        int min = Integer.MAX_VALUE;
+        int sum = 0;
+        for (int i = 1; i*i <= n; i++) {
+            if (i * i >= m && i * i <= n) {
+                min = Math.min(i*i, min);
+                sum += i*i;
+            }
+        }
+        if (sum == 0) {
+            System.out.print(-1);
+        } else {
+            System.out.print(sum + "\n" + min);
+        }
+    }
+}
