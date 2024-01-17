edb0b45432cf0ba5a30db5a22b7b43e6afb6c91e
JeongWonYoung <nicednjsdud@naver.com>
2024-01-17 22:35:50 +0900

    Solve binary search 1920

---
+++ src/Back_1920_1.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Back_1920_1 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            System.out.println(binarySearch(arr, num));
+        }
+    }
+
+    private static Integer binarySearch(int[] arr, int num) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (arr[mid] == num) return 1;
+            else if (arr[mid] > num) end = mid - 1;
+            else if (arr[mid] < num) start = mid + 1;
+        }
+        return 0;
+    }
+}
