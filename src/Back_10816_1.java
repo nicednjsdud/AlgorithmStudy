94ec7001ae3f5d6d6792ffd14bd273c943edc5b6
JeongWonYoung <nicednjsdud@naver.com>
2024-01-17 23:30:06 +0900

    Solve algorithm 10816

---
+++ src/Back_10816_1.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Back_10816_1 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int arr[] = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            int num = Integer.parseInt(st.nextToken());
+
+
+            System.out.print(upperBound(arr, num) - lowerBound(arr, num) + " ");
+        }
+    }
+    private static int upperBound(int[] arr, int num) {
+        int start = 0;
+        int end = arr.length;
+        while (start < end) {
+            int mid = (start + end) / 2;
+            if (arr[mid] > num) end = mid;
+            else start = mid + 1;
+        }
+        return start;
+    }
+    private static int lowerBound(int[] arr, int num) {
+        int start = 0;
+        int end = arr.length;
+        while (start < end) {
+            int mid = (start + end) / 2;
+            if (arr[mid] >= num) end = mid;
+            else start = mid + 1;
+        }
+        return start;
+    }
+
+
+}
