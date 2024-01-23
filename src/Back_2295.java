358ea99ad5d6d0013fdfe5986af459761af255a5
JeongWonYoung <nicednjsdud@naver.com>
2024-01-23 23:52:16 +0900

    Solve binary search with implement

--- src/Back_2295.java
+++ src/Back_2295.java
@@ -1,44 +1,56 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
+
+/**
+ * 이분탐색 ( 주의 이분탐색은 항상 정렬을 해야함 )
+ * A + B 를 먼저 더해 집합 list를 만든뒤 (list)
+ * C를 대입하여 이전 arr에 가장 큰수를 구한다.
+ */
 public class Back_2295 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        int[] arr = new int[N];
-        int[] twoArr = new int[N];
+        int arr[] = new int[N];
+        List<Integer> list = new ArrayList<>();
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(br.readLine());
         }
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                list.add(arr[i] + arr[j]);
+            }
+        }
         Arrays.sort(arr);
-        for (int i = 0; i < N; i++) {
-            for (int j = i; j < N; j++) {
-                twoArr[i] = arr[i] + arr[j];
+        Collections.sort(list);
+        for (int i = N - 1; i >= 0; i--) {
+            for (int j = 0; j < N; j++) {
+                int gap = arr[i] - arr[j];
+                if (binarySearch(list, gap)) {
+                    System.out.println(arr[i]);
+                    return;
+                }
             }
         }
-        Arrays.sort(twoArr);
-        int sum = 0;
-        for (int i = 0; i < N; i++) {
-            int searchMax = (binarySearch(twoArr, arr[i]));
-            if(sum < searchMax){
-                sum = searchMax;
-            }
-        }
-        System.out.println(sum);
+        br.close();
     }
 
-    private static int binarySearch(int[] twoArr, int num) {
-        int start = 0;
-        int end = twoArr.length - 1;
+    private static boolean binarySearch(List<Integer> list, int gap) {
+        int min = 0;
+        int max = list.size() - 1;
 
-        while (start <= end) {
-            int mid = (start + end) / 2;
-            if (twoArr[mid] == num) return num;
-            else if (twoArr[mid] > num) end = mid - 1;
-            else if (twoArr[mid] < num) start = mid + 1;
+        while (min < max) {
+            int mid = (max + min) / 2;
+            if(list.get(mid) == gap) return true;
+            else if(list.get(mid) > gap) max = mid -1;
+            else min = mid + 1;
         }
-        return 0;
+        return false;
     }
+
 }
