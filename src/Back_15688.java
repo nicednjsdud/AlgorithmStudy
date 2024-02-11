cf8051ae30a4acdefb849cde369a3f1ec550f289
JeongWonYoung <nicednjsdud@naver.com>
2024-02-11 22:24:30 +0900

    Feat: 시간초과로 인한 StringBuilder사용

--- src/Back_15688.java
+++ src/Back_15688.java
@@ -2,21 +2,25 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class Back_15688 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         int N = Integer.parseInt(br.readLine());
-        List<Integer> list = new ArrayList<>();
+       int arr[] = new int[N];
         for(int i = 0; i < N; i++){
-            list.add(Integer.parseInt(br.readLine()));
+            arr[i] = Integer.parseInt(br.readLine());
         }
-        // list 내림차순
-        Collections.sort(list);
-        for (Integer i : list) {
-            System.out.println(i);
+        Arrays.sort(arr);
+
+        for(int i = 0; i < N; i++){
+            sb.append(arr[i]).append("\n");
         }
+        System.out.println(sb);
+        br.close();
     }
 }
