ca5a6f03663f8ee6d7d332be4651b53e0e25bf8c
JeongWonYoung <nicednjsdud@naver.com>
2024-04-05 22:44:23 +0900

    Solve algorithm

---
+++ src/Back_15688_2.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Back_15688_2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[2000001];
+      
+        for (int i = 0; i < N; i++) {
+            arr[Integer.parseInt(br.readLine()) + 1000000]++;
+        }
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] != 0){
+                for(int j = 0; j < arr[i]; j++){
+                    System.out.println(i - 1000000);
+                }
+            }
+        }
+    }
+}
