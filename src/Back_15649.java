88ff7b06c977df51f46ca19c9297b6d330b3b0b0
JeongWonYoung <nicednjsdud@naver.com>
2024-06-04 23:52:12 +0900

    no message

--- src/Back_15649.java
+++ src/Back_15649.java
@@ -1,12 +1,14 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Back_15649 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        int M = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
 
         int[] arr = new int[N];
         boolean[] visited = new boolean[N];
