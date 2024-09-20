9a6b39439c00fd03b0b3108d9d1870aa4a721f17
JeongWonYoung <nicednjsdud@naver.com>
2024-09-20 16:01:15 +0900

    solve algorithm

---
+++ src/Back_1644.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Back_1644 {
+
+    static int n;
+    static boolean[] isPrime;
+    static List<Integer> list;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+
+        makePrime();
+
+        list = new ArrayList<>();
+        for(int i = 2; i <= n; i++){
+            if(!isPrime[i]) list.add(i);
+        }
+
+        int en = 0, count = 0;
+        for(int st = 0; st < list.size(); st++){
+            int sum = 0;
+            en = st;
+            while(en < list.size() && sum < n){
+                sum += list.get(en);
+                en++;
+            }
+            if(sum == n) count++;
+        }
+        System.out.println(count);
+    }
+
+    static void makePrime() {
+        isPrime = new boolean[n+1];
+        isPrime[0] = isPrime[1] = true;
+        for (int i = 2; i*i <= n; i++) {
+            if (isPrime[i]) continue;
+            for (int j = i*i; j <= n; j+=i) {
+                isPrime[j] = true;
+            }
+        }
+    }
+}
