bfab2f0e635e1e601edf39a6d3af8779ee1a0af0
JeongWonYoung <nicednjsdud@naver.com>
2025-04-25 17:09:17 +0900

    solve algorithm

---
+++ src/Back_15657.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Back_15657 {
+
+    static int N, M;
+    static int[] input;
+    static int[] output;
+
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        input = new int[N];
+        output = new int[M];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            input[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(input); // 사전순 출력을 위해 정렬
+        dfs(0, 0);
+        System.out.print(sb);
+    }
+
+    private static void dfs(int start, int depth) {
+        if (depth == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(output[i]).append(" ");
+            }
+            sb.append('\n');
+            return;
+        }
+
+        for (int i = start; i < N; i++) {
+            output[depth] = input[i];
+            dfs(i, depth + 1);
+        }
+    }
+}
